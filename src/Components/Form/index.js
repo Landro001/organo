6a8d1acd7f54639381c2a00ffe15c79ce85b5f19
Leandro <ljborges76@gmail.com>
2024-06-25 11:35:39 -0300

    Lista dos times

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,7 +1,19 @@
+import DropDown from '../DropDown'
 import TextField from '../TextField'
 import './Form.css'
 
 const Form = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
     return(
         <section className='form'>
             <form>
@@ -9,6 +21,7 @@ const Form = () => {
                 <TextField label="Nome" placeholder="Digite seu nome"/>
                 <TextField label="Cargo" placeholder="Digite seu cargo"/>
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <DropDown label="Times" itens={times}/>
             </form>
         </section>
     )
