6bec357e5b93a357123151a3e543fa67d437f434
Landro <ljborges76@gmail.com>
2024-06-28 09:53:52 -0300

    Card de colaboradores

---
+++ src/Components/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src="https://github.com/Landro001.png" alt="Leandro Borges" />
+            </div>
+            <div className='rodape'>
+                <h4>Leandro Borges</h4>
+                <h5>Engenheiro de Software</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
--- src/Components/Team/index.js
+++ src/Components/Team/index.js
@@ -1,9 +1,12 @@
+import Colaborador from '../Colaborador'
 import './Team.css'
 
 const Team = (props) => {
     return (
         <section className='team' style={{ backgroundColor: props.corSecundaria }}>
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <Colaborador />
+            <Colaborador />
         </section>
     )
 }
